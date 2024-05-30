f16d8424bc2283aa880c94f09ea4c6231f0b7b84
grac-e <adelowotang@gmail.com>
2024-05-30 15:54:00 +0200

    responsive cv

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import handlebars from 'vite-plugin-handlebars';
 import data from './data.json';
 
 export default {
+  base : '/responsive_cv/',
   plugins: [
     handlebars({
       context: data, // replace the curly brackets with data.json
